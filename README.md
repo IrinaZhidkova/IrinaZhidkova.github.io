3cbd9e141e00ae85f0a592d63f77faf35fd4b5de
Ira <49819439+IrinaZhidkova@users.noreply.github.com>
2025-02-19 19:50:46 +0100

    CV files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# IrinaZhidkova.github.io
+# cv_test
+Сайт-визика на HTML + CSS + JS
---
+++ js.js
@@ -0,0 +1,7 @@
+/* Это объявление переменной, мы наши кнопку по тегу */
+const button = document.querySelector('button');
+
+/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
+button.addEventListener('click', function() {
+	alert('My therapist: so what have you been up to since our last session? \nMe: haha you know general bug fixes and performance improvements')
+})
